c381df975e2b2134dde3518c944c1539c52b9e86
Gayane25 <gayabennu@gmail.com>
2022-01-11 20:06:38 +0400

    feat:added Book class and its methods

--- AuthorBook.js
+++ AuthorBook.js
@@ -28,7 +28,45 @@ class Author {
   }
 }
 
+class Book {
+  constructor(title, author, price, quantity) {
+    this._title = title;
+    this._author = author;
+    this.price = price;
+    this.quantity = quantity;
+  }
+  get title() {
+    return this._title;
+  }
+  set title(newTitle) {
+    this._title = newTitle;
+    return this._title;
+  }
+  get author() {
+    return this._author;
+  }
+  set author(val) {
+    this._author = val;
+    return this._author;
+  }
+  getProfit() {
+    if ((typeof this.price && typeof this.quantity) !== 'number') {
+      throw new Error('Only Numbers!!');
+    }
+    return this.price * this.quantity;
+  }
+}
+
 const Rowling = new Author('J.K.Rowling', 'anc@gmail.com', 'female');
 console.log(Rowling.toString());
 console.log(Rowling.name);
 console.log(Rowling.gender);
+const Dambldore = new Book(
+  'The Secrets of Dambledore',
+  'J.K.Rowling',
+  6000,
+  10000
+);
+console.log(Dambldore.title);
+console.log(Dambldore.quantity);
+console.log(Dambldore.getProfit());
