8a59d0be7c8fa3476bbf21f556a1915397a6eceb
Gayane25 <gayabennu@gmail.com>
2022-01-13 00:10:42 +0400

    feat:solved partly,required to continue

---
+++ RequiredTasks/Library.js
@@ -0,0 +1,200 @@
+"use strict";
+
+class Library {}
+
+
+class Book {
+  constructor(title, author) {
+    this._title = title
+    this._author = author
+  }
+  get title() {
+    return this._title;
+  }
+  set title(onlystr) {
+    if (typeof onlystr !== 'string') {
+      throw new Error('this field must be String');
+    }
+    this._title = onlystr;
+  }
+  get author() {
+    return this._author
+  }
+
+  set author(onlystr) {
+    if (typeof onlystr !== 'string') {
+      throw new Error('this field must be String');
+    }
+    this._author = onlystr;
+  }
+
+  toString() {
+    return `${this._title} of ${this._author}`
+  }
+
+  isTheSameBook(book) {
+    if ((book.title === this._title) && (book.author === this._author)) {
+      return true;
+    }
+  }
+}
+
+class LibraryBookBase extends Book {
+  constructor(title, author, bookId) {
+    super(title, author)
+    this._bookId = bookId
+  }
+
+  get bookId() {
+    return this._bookId
+  }
+
+  set bookId(onlyNum) {
+    if (typeof onlyNum !== 'number') {
+      throw new Error('this field must be only number');
+    }
+    this._bookId = onlyNum;
+  }
+  toString() {
+    return `${this._title} ${this._author} ${this._bookId}`
+  }
+}
+
+class LibraryBook extends Book {
+  constructor(title, author, bookId, quantity) {
+    super(title, author)
+    this._bookId = bookId
+    this._quantity = quantity
+  }
+  get bookId() {
+    return this._bookId
+  }
+  set bookId(onlyNum) {
+    if (typeof onlyNum !== 'number') {
+      throw new Error('this field must be only number');
+    }
+    this._bookId = onlyNum;
+  }
+
+  get quantity() {
+    return this._quantity
+  }
+  set quantity(onlyNum) {
+    if (typeof onlyNum !== 'number') {
+      throw new Error('this field must be only number');
+    }
+    this._quantity = onlyNum;
+  }
+
+  toString() {
+    return `${this._title}, ${this._author}, ${this._bookId}, ${this._quantity}`
+  }
+
+  increaseQuantityBy(amount) {
+    if (typeof amount !== "number") {
+      throw new Error("only number");
+    }
+    this._quantity += amount;
+    return this._quantity;
+  }
+
+  decreaseQuantityBy(amount) {
+    if (typeof amount !== "number") {
+      throw new Error("only number");
+    }
+    if (amount > this._quantity) {
+      throw new Error("amount must be less than quantity");
+    }
+    this._quantity -= amount;
+    return this._quantity;
+  }
+}
+
+class ReaderBook extends Book {
+  constructor(title, author, bookId, expirationDate, isReturned) {
+    super(title, author)
+    this._bookId = bookId
+    this._expirationDate = expirationDate
+    this._isReturned = isReturned
+  }
+  get bookId() {
+    return this._bookId
+  }
+  set bookId(onlyNum) {
+    if (typeof onlyNum !== 'number') {
+      throw new Error('this field must be only number');
+    }
+    this._bookId = onlyNum;
+  }
+  get expirationDate() {
+    return this._expirationDate
+  }
+  set expirationDate(onlystr) {
+    if (typeof onlystr !== 'string') {
+      throw new Error('this field must be String');
+    }
+    this._expirationDate = onlystr;
+  }
+  get isReturned() {
+    return this._isReturned
+  }
+  set isReturned(checking) {
+    if ((this.isReturned === true) || (this.isReturned === "returned")) {
+      this._isReturned = true
+      return this._isReturned
+    }
+    if ((this.isReturned === false) || (this.isReturned === "isNotReturned")) {
+      this._isReturned = false
+      return this._isReturned
+    }
+  }
+  toString() {
+    return `${this._title}, ${this._author}, ${this._bookId}, ${this._expirationDate}, ${this._isReturned}`
+  }
+}
+
+class Reader {
+  static id = 0;
+  constructor(firstName, lastName, books) {
+    this._id = ++Reader.id
+    this._firstName = firstName
+    this._lastName = lastName
+    this._books = books
+  }
+  get firstName() {
+    return this._firstName
+  }
+  set firstName(strOnly) {
+    if (typeof strOnly !== "string") {
+      throw new Error("this field must be string")
+    }
+    this._firstName = strOnly
+  }
+  get lastName() {
+    return this._lastName
+  }
+  set lastName(strOnly) {
+    if (typeof strOnly !== "string") {
+      throw new Error("this field must be string")
+    }
+    this._lastName = strOnly
+  }
+  get books() {
+    return this._books
+  }
+  set books(checkArr) {
+    if (!Array.isArray(checkArr)) {
+      throw new Error("this field type must be array")
+    }
+    this._books = checkArr;
+  }
+  toString() {
+    return `${this._firstName}, ${this._lastName}, ${this._books}`
+  }
+
+  borrowBook(book, library) {
+    if (library.includes(book) && book instanceof ReaderBook) {
+      this._books.push(book);
+    }
+
+  }
