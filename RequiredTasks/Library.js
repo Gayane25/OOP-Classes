1778f56291cf344f626a70d3cf368f3521508f3d
Gayane25 <gayabennu@gmail.com>
2022-01-13 14:52:29 +0400

    fix: changed features

--- RequiredTasks/Library.js
+++ RequiredTasks/Library.js
@@ -42,23 +42,13 @@ class Book {
 }
 
 class LibraryBookBase extends Book {
-  constructor(title, author, bookId) {
+  static id = 0;
+  constructor(title, author) {
     super(title, author);
-    this._bookId = bookId;
-  }
-
-  get bookId() {
-    return this._bookId;
-  }
-
-  set bookId(onlyNum) {
-    if (typeof onlyNum !== 'number') {
-      throw new Error('this field must be only number');
-    }
-    this._bookId = onlyNum;
+    this._bookId = ++LibraryBookBase.id;
   }
   toString() {
-    return `${this._title} ${this._author} ${this._bookId}`;
+    return `${this._title} ${this._author}`;
   }
 }
 
